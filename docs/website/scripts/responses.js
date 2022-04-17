cab98fed376bdcdc0c9d98c018bd4f0b1ceb009f
SPRMNI <hky10242@my.yorku.ca>
2022-04-17 16:11:35 -0400

    phase3

---
+++ docs/website/scripts/responses.js
@@ -0,0 +1,19 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "cyble") {
+        return "Life!";
+    } else if (input == "need") {
+        return "peace, food, water";
+    } else if (input == "life") {
+        return "will become better and better";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
